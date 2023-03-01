82c83ec59c539a53f4f8804f6458c1906e4d29b4
paulusbakker <paulusbakker@gmail.com>
2023-03-01 11:27:06 +0100

    index.tsx working

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-
+// import "./index.css";
 import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement!);
-
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+
+// reportWebVitals();
